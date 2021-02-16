89d6d5e86508bc5321023df561235b483cb0450a
Peter-Bahrynovskyi <76909881+Peter-Bahrynovskyi@users.noreply.github.com>
2021-02-16 22:03:06 +0200

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,15 @@
+let modal = document.getElementById("my_modal");
+let btn = document.getElementById("btn_modal_window");
+let span = document.getElementsByClassName("close_modal_window")[0];
+
+btn.onclick = function () {
+   modal.style.display = "block";
+}
+span.onclick = function () {
+   modal.style.display = "none";
+}
+window.onclick = function (event) {
+   if (event.target === modal) {
+       modal.style.display = "none";
+   }
+}
