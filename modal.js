6d530eea6a5cbdf5850043adee97056f437f8b95
Peter-Bahrynovskyi <76909881+Peter-Bahrynovskyi@users.noreply.github.com>
2021-02-16 23:35:44 +0200

    Add files via upload

--- modal.js
+++ modal.js
@@ -1,15 +1,32 @@
-let modal = document.getElementById("my_modal");
-let btn = document.getElementById("btn_modal_window");
-let span = document.getElementsByClassName("close_modal_window")[0];
+let nav_modal = document.getElementById("nav_modal");
+let nav_btn = document.getElementById("btn_modal_nav");
+let nav_span = document.getElementsByClassName("close_modal_nav")[0];
 
-btn.onclick = function () {
-   modal.style.display = "block";
+nav_btn.onclick = function () {
+   nav_modal.style.display = "block";
 }
-span.onclick = function () {
-   modal.style.display = "none";
+nav_span.onclick = function () {
+   nav_modal.style.display = "none";
 }
 window.onclick = function (event) {
-   if (event.target === modal) {
-       modal.style.display = "none";
+   if (event.target === nav_modal) {
+      nav_modal.style.display = "none";
+   }
+}
+
+
+let about_modal = document.getElementById("about_modal");
+let about_btn = document.getElementById("btn_modal_about");
+let about_span = document.getElementsByClassName("close_modal_about")[0];
+
+about_btn.onclick = function () {
+   about_modal.style.display = "block";
+}
+about_span.onclick = function () {
+   about_modal.style.display = "none";
+}
+window.onclick = function (event) {
+   if (event.target === about_modal) {
+      about_modal.style.display = "none";
    }
 }
